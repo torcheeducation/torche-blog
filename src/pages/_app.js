dbf830261416070951cc66ff4cd12e9ec92f4d0e
Muhammad Naufal Farras <opal.farras@gmail.com>
2023-03-07 17:04:56 +0700

    add app.manifest & service worker

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,26 @@
 import '@/styles/globals.css'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const registerSW = async () => {
+      if (!('serviceWorker' in navigator)) {
+        console.log('Browser tidak mendukung Service Worker')
+        return
+      }
+  
+      try {
+        await navigator.serviceWorker.register('/sw.js')
+        console.log('Service Worker registered')
+      } catch (error) {
+        console.log('Failed to register Service Worker', error)
+      }
+    }
+    
+    window.addEventListener('load', () => {
+      registerSW()
+    })
+  }, [])
+
   return <Component {...pageProps} />
 }

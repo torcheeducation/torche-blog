c8ba262d1e282ad2a1d93d4f5ea28243690888bb
Muhammad Naufal Farras <opal.farras@gmail.com>
2023-03-14 08:28:22 +0700

    add new posts page

---
+++ src/pages/posts/index.js
@@ -0,0 +1,10 @@
+import Layout from "@/components/Layout";
+import NewPosts from "@/components/NewPosts";
+
+export default function Posts() {
+  return (
+    <Layout title="Postingan">
+      <NewPosts />
+    </Layout>
+  )
+}

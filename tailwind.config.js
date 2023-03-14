2cd0ef585a9ed0de9b084716f3b601e473e84ff4
Muhammad Naufal Farras <opal.farras@gmail.com>
2023-03-14 10:43:49 +0700

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,12 +18,6 @@ module.exports = {
         rajdhaniMedium: '"Rajdhani Medium", sand-serif',
         rajdhaniBold: '"Rajdhani Bold", sans-serif'
       },
-      dropShadow: {
-        blue: [
-          '0 20px 13px rgb(88, 133, 233 / 0.7)', 
-          '0 8px 5px rgb(88, 133, 233 / 0.8)',
-        ],
-      },
     },
   },
   plugins: [

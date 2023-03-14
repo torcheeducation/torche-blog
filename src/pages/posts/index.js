8e6826ac2e620cd23cf9c9aa387a7fcf6ef3fba9
Muhammad Naufal Farras <opal.farras@gmail.com>
2023-03-14 10:42:09 +0700

    update postspage UI & features, update Homepage UI

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,10 +1,14 @@
+import AllPosts from "@/components/AllPosts";
 import Layout from "@/components/Layout";
 import NewPosts from "@/components/NewPosts";
 
 export default function Posts() {
   return (
     <Layout title="Postingan">
-      <NewPosts />
+      <div className="w-full px-4 py-6 xl:px-14">
+        <NewPosts />
+        <AllPosts />
+      </div>
     </Layout>
   )
 }

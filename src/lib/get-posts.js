024fd5e6eb872514c308956e993dc011cb8eec0b
Muhammad Naufal Farras <opal.farras@gmail.com>
2023-03-28 14:17:54 +0700

    Update add post form & Update SignUp Page

---
+++ src/lib/get-posts.js
@@ -0,0 +1,6 @@
+export async function getPosts() {
+  const res = await fetch("/api/posts")
+  const data = await res.json()
+
+  return data
+}
--- src/models/post.js
+++ src/models/post.js
@@ -21,6 +21,10 @@ const postSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please enter the owner of this post"]
   },
+  visitor: {
+    type: Number,
+    required: [true, "Please enter the number of visitor in this post"]
+  },
   createdAt: {
     type: Date,
     default: Date.now,

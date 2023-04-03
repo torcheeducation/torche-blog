c91bc850ae24b093455ccdf8ec80fe4b96de18eb
Muhammad Naufal Farras <opal.farras@gmail.com>
2023-04-03 09:55:50 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,10 +5,8 @@ module.exports = withWorkbox({
   reactStrictMode: true,
   images: {
     domains: [
-      `${process.env.S3_UPLOAD_BUCKET}.s3.amazonaws.com`,
-      `${process.env.S3_UPLOAD_BUCKET}.s3.${process.env.S3_UPLOAD_REGION}.amazonaws.com`,
-      'torche-blog-post-images.s3.ap-southeast-1.amazonaws.com',
-      'torche-blog-post-images.s3.amazonaws.com'
+      `${process.env.NEXT_PUBLIC_S3BUCKET}.s3.amazonaws.com`,
+      `${process.env.NEXT_PUBLIC_S3BUCKET}.s3.${process.env.NEXT_PUBLIC_AWSREGION}.amazonaws.com`
     ],
   },
 })

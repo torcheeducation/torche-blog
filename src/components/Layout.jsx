ce34f63920b092fa8db38398fc9b2b87c6daf7ce
Muhammad Naufal Farras <opal.farras@gmail.com>
2023-05-27 13:56:12 +0700

    Change description of page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,7 +9,7 @@ export default function Layout({ children, title }) {
   return (
     <>
       <Head>
-        <meta name="description" content="Sekumpulan bacaan mengenai teknik kimia" />
+        <meta name="description" content="Torche Education adalah perusahaan startup teknologi pendidikan yang berbasis di Indonesia, dengan fokus pada teknik kimia, teknik bioproses, dan mata pelajaran teknik proses lainnya." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#0B122A" />
         <link rel="icon" href="/favicon.ico" />

dd0ea50a388c4bf9c064dcad5f62ba7b91d5774a
Muhammad Naufal Farras <opal.farras@gmail.com>
2023-06-23 14:37:31 +0700

    Update User API

--- src/pages/api/auth/signup.js
+++ src/pages/api/auth/signup.js
@@ -7,7 +7,8 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
       const user = await User.create(req.body)
-      res.status(201).json({ 
+      res.status(201).json({
+        status: "success",
         message: "Created user!",
         data: {
           id: user._id,
@@ -16,18 +17,57 @@ export default async function handler(req, res) {
         },
       })
     } catch (error) {
+      if (error.keyValue.username) {
+        res.status(400).json({
+          status: "fail",
+          message: "username already used",
+        });
+        return
+      }
+
       console.log(error)
-      res.status(400).json({ message: error })
+      res.status(400).json({ status: "fail", message: error })
     }
   } else if (req.method === "GET") {
     try {
       const user = await User.find()
       res.status(200).json({
+        status: "success",
         user,
       })
     } catch (error) {
       console.log(error)
-      res.status(404).json({ message: error })
+      res.status(404).json({ status: "fail", message: error })
+    }
+  } else if (req.method === "DELETE") {
+    try {
+      const id = req.body.id;
+
+      if (!id) {
+        res.status(400).json({
+          status: "fail",
+          message: "id payload needed"
+        });
+        return
+      }
+
+      const user = await User.findByIdAndDelete(id);
+      
+      if (!user) {
+        res.status(404).json({
+          status: "fail",
+          message: "user not found"
+        });
+        return
+      }
+
+      res.status(200).json({
+        status: "success",
+        message: `Delete User ${id} success`,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({ status: "fail", message: error });
     }
   } else {
     res.status(404).json({ status: "error", message: "Method not found!" })
